f60741665162abb171b8b8053bc7bb59f2bbf026
MengKun <mengkun823@163.com>
2017-08-15 19:24:19 +0800

    shangxian

---
+++ src/滚动条.js
@@ -0,0 +1,20 @@
+$(function() {
+    var winHeight = $(document).scrollTop();
+
+    $(window).scroll(function() {
+        var scrollY = $(document).scrollTop(); // 获取垂直滚动的距离，即滚动了多少
+
+        if (scrollY > 550) { //如果滚动距离大于550px则隐藏，否则删除隐藏类
+            $('#pageDown').addClass('hiddened');
+        } else {
+            $('#pageDown').removeClass('hiddened');
+        }
+
+        // if (scrollY > winHeight) { //如果没滚动到顶部，删除显示类，否则添加显示类
+        //     $('.top-title').removeClass('showed');
+        // } else {
+        //     $('.top-title').addClass('showed');
+        // }
+
+    });
+});
